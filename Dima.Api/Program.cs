6ae0ee8c83628b96c826f56c911db01ba7a9e99c
Aleksei da Silva <alekseidasilva@hotmail.com>
2024-06-07 13:29:56 +0100

    :sparkles: Requests

--- Dima.Api/Program.cs
+++ Dima.Api/Program.cs
@@ -3,7 +3,29 @@ var app = builder.Build();
 
 
 //Endpoints->Url para Acesso
-app.MapGet("/", () => "Hello World!");
+app.MapPost(
+    "/v1",
+    () => "Hello World!");
+
+
 
 
 app.Run();
+
+//Request
+public class Request()
+{
+    public long Id { get; set; }
+    public string Title { get; set; }=String.Empty;
+    
+    public DateTime CreatedAt { get; set; }=DateTime.Now;
+    public DateTime? PaidOrReceivedAt { get; set; } = null;
+
+    public ETransationType Type { get; set; } = ETransationType.Withdraw;
+    public decimal Amount { get; set; }
+    public long CategoryId { get; set; }
+    public Category Category { get; set; } = null!;
+    public string UserId { get; set; }=String.Empty;
+}
+//Response
+//Hanbler
