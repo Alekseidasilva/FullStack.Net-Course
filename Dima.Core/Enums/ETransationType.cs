fb33630437076f59b2e2d4eaa3d3539e78530201
Aleksei da Silva <alekseidasilva@hotmail.com>
2024-06-07 12:53:47 +0100

    Add Project Folders and Class Enums, Models

---
+++ Dima.Core/Enums/ETransationType.cs
@@ -0,0 +1,7 @@
+﻿namespace Dima.Core.Enums;
+
+public enum ETransationType
+{
+    Deposit=1,
+    Withdraw=2
+}
---
+++ Dima.Core/Models/Transation.cs
@@ -0,0 +1,18 @@
+﻿using Dima.Core.Enums;
+
+namespace Dima.Core.Models;
+
+public class Transation
+{
+    public long Id { get; set; }
+    public string Title { get; set; }=String.Empty;
+    
+    public DateTime CreatedAt { get; set; }=DateTime.Now;
+    public DateTime? PaidOrReceivedAt { get; set; } = null;
+
+    public ETransationType Type { get; set; } = ETransationType.Withdraw;
+    public decimal Amount { get; set; }
+    public long CategoryId { get; set; }
+    public Category Category { get; set; } = null!;
+    public string UserId { get; set; }=String.Empty;
+}
