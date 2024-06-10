ed1e1453e83de08246447ab67330cd4dd629fe23
Aleksei da Silva <alekseidasilva@hotmail.com>
2024-06-10 22:50:12 +0100

    :sparkles: M10A03 - IdentityDbContext

--- Dima.Api/Data/AppDbContext.cs
+++ Dima.Api/Data/AppDbContext.cs
@@ -1,11 +1,22 @@
+using Dima.Api.Models;
 using Dima.Core.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System.Reflection;
 
 namespace Dima.Api.Data;
 
-public class AppDbContext(DbContextOptions<AppDbContext> options) : IdentityDbContext(options)
+public class AppDbContext(DbContextOptions<AppDbContext> options)
+    : IdentityDbContext<
+        User,
+        IdentityRole<long>,
+        long,
+        IdentityUserClaim<long>,
+        IdentityUserRole<long>,
+        IdentityUserLogin<long>,
+        IdentityRoleClaim<long>,
+        IdentityUserToken<long>>(options)
 {
     //public AppDbContext(DbContextOptions<AppDbContext> options):base(options) { }
 
