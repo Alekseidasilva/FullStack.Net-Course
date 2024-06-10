f58473f2117122872cb6be18a0f1267318054a36
Aleksei da Silva <alekseidasilva@hotmail.com>
2024-06-10 19:43:55 +0100

    Create TransationHandler.cs

---
+++ Dima.Api/Handlers/TransationHandler.cs
@@ -0,0 +1,51 @@
+﻿using Dima.Api.Data;
+using Dima.Core.Handlers;
+using Dima.Core.Models;
+using Dima.Core.Request.Transations;
+using Dima.Core.Response;
+
+namespace Dima.Api.Handlers;
+
+public class TransationHandler(AppDbContext context) : ITransationHandler
+{
+    public async Task<Response<Transation?>> CreateAsync(CreateTransationRequest request)
+    {
+        var transation = new Transation
+        {
+            UserId = request.UserId,
+            CategoryId = request.CategoryId,
+            CreatedAt = DateTime.Now,
+            Amount = request.Amount,
+            PaidOrReceivedAt = request.PaidOrReceivedAt,
+            Title = request.Title,
+            Type = request.Type
+        };
+        await context.Transations.AddAsync(transation);
+        await context.SaveChangesAsync();
+        return new Response<Transation?>(transation, 201, "Transacao criada com Sucesso!");
+    }
+
+    public async Task<Response<Transation?>> UpdateAsync(UpdateTransationRequest request)
+    {
+        var transation = await context.Transations.FindAsync(request.Id);
+        if (transation == null)
+            return new Response<Transation?>(null, 404, "Transacao nao encontrada!");
+        transation.Title = request.Title;
+
+    }
+
+    public async Task<Response<Transation?>> DeleteAsync(DeleteTransationRequest request)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<Response<Transation?>> GetByIdAsync(GetTransationByIdRequest request)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<PagedResponse<List<Transation>>> GetByPeriodAsync(GetTransationByPeriodRequest request)
+    {
+        throw new NotImplementedException();
+    }
+}
