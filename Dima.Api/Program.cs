5816497222a246ce388734e11cefb1195bc2e2e0
Aleksei da Silva <44441672+Alekseidasilva@users.noreply.github.com>
2024-06-11 13:08:04 +0100

    :sparkles: M10A09 - LogOut

--- Dima.Api/Program.cs
+++ Dima.Api/Program.cs
@@ -1,3 +1,4 @@
+using System.Security.Claims;
 using Dima.Api.Common.EndPoints;
 using Dima.Api.Data;
 using Dima.Api.Handlers;
@@ -47,6 +48,42 @@ app.MapEndPoints();
 app.MapGroup("v1/identity")
     .WithTags("Identity")
     .MapIdentityApi<User>();
+
+app.MapGroup("v1/identity")
+    .WithTags("Identity")
+    .MapPost("/logout", async (
+        SignInManager<User> signInManager) =>
+    {
+        await signInManager.SignOutAsync();
+        return Results.Ok();
+
+    })
+    .RequireAuthorization();
+app.MapGroup("v1/identity")
+    .WithTags("Identity")
+    .MapPost("/roles", async (ClaimsPrincipal user
+     ) =>
+    {
+
+        if (user.Identity is null
+            || !user.Identity.IsAuthenticated)
+            return Results.Unauthorized();
+
+        var identity = (ClaimsIdentity)user.Identity;
+        var roles = identity
+            .FindAll(identity.RoleClaimType)
+            .Select(c=>new
+            {
+                c.Issuer,
+                c.OriginalIssuer,
+                c.Type,
+                c.ValueType
+            });
+        
+        return TypedResults.Json(roles);
+
+    })
+    .RequireAuthorization();
 app.Run();
 
 
