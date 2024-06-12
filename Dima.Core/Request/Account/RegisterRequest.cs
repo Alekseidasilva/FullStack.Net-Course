1366552238714df71f7d7ed5d9a160a4d0ec6660
Aleksei da Silva <alekseidasilva@hotmail.com>
2024-06-12 10:13:12 +0100

    FEAT: M14A02 - Definindo os Servicos

---
+++ Dima.Core/Request/Account/RegisterRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Dima.Core.Request.Account;
+
+public class RegisterRequest : Request
+{
+    [Required(ErrorMessage = "O Campo email e obrigatorio")]
+    [EmailAddress(ErrorMessage = "Email Invalido")]
+    public string Email { get; set; } = String.Empty;
+
+    [Required(ErrorMessage = "O Campo senha e obrigatorio")]
+    public string Password { get; set; } = String.Empty;
+}
