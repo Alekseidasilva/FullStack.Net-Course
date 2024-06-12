45a41e3c6193ef0077a367a185e62eaaa2ce6b99
Aleksei da Silva <alekseidasilva@hotmail.com>
2024-06-12 10:31:23 +0100

    FEAT: M14A03 - httpClient

--- Dima.Web/Configuration.cs
+++ Dima.Web/Configuration.cs
@@ -5,7 +5,8 @@ namespace Dima.Web;
 
 public static class Configuration
 {
-
+    public const string HttpClientName = "dima";
+    public static string BackEndUrl { get; set; } = "https://localhost:7258";
     public static MudTheme Theme = new()
     {
         Typography = new Typography
