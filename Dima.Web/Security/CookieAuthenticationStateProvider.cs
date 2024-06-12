d2deffdbeda89d8fba1b9c4764cd722da0c5d507
Aleksei da Silva <alekseidasilva@hotmail.com>
2024-06-12 11:36:44 +0100

    FEAT: M14A07 - Autentication State Provider

---
+++ Dima.Web/Security/CookieAuthenticationStateProvider.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+
+namespace Dima.Web.Security;
+
+public class CookieAuthenticationStateProvider(IHttpClientFactory clientFactory) : AuthenticationStateProvider, ICookieAuthenticationStateProvider
+{
+    private readonly HttpClient _client = clientFactory.CreateClient(Configuration.HttpClientName);
+    private bool IsAuthenticated = false;
+
+    public async Task<bool> CheckAuthenticatedAsync()
+    {
+        await GetAuthenticationStateAsync();
+        return IsAuthenticated;
+    }
+
+    public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void NotifyAuthenticationStateChanged()
+    => NotifyAuthenticationStateChanged(GetAuthenticationStateAsync());
+
+}
