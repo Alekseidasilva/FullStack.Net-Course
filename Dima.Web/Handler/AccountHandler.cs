ac047e6eea1e8c4402f9966642f59a622cfa6092
Aleksei da Silva <alekseidasilva@hotmail.com>
2024-06-12 10:44:49 +0100

    FEAT: M14A04 - HttpFactory

---
+++ Dima.Web/Handler/AccountHandler.cs
@@ -0,0 +1,25 @@
+﻿using Dima.Core.Handlers;
+using Dima.Core.Request.Account;
+using Dima.Core.Response;
+using System.Net.Http.Json;
+
+namespace Dima.Web.Handler;
+
+public class AccountHandler(IHttpClientFactory hppClientFactory) : IAccountHandler
+{
+    private readonly HttpClient _client = hppClientFactory.CreateClient(Configuration.HttpClientName);
+    public async Task<Response<string>> LoginAsync(LoginRequest request)
+    {
+        await _client.PostAsJsonAsync("/v1/identity/login", request);
+    }
+
+    public async Task<Response<string>> RegisterAsync(RegisterRequest request)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task LogoutAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
