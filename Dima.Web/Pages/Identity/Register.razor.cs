493a9301e5aba66ee369fcf37a55a75b7c431106
Aleksei da Silva <alekseidasilva@hotmail.com>
2024-06-12 15:49:43 +0100

    FEAT: M14A16 - Inject

--- Dima.Web/Pages/Identity/Register.razor.cs
+++ Dima.Web/Pages/Identity/Register.razor.cs
@@ -1,9 +1,13 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using Dima.Web.Security;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
 
 namespace Dima.Web.Pages.Identity;
 
 
 public partial class RegisterPage : ComponentBase
 {
-
+    [Inject]
+    public CookieAuthenticationStateProvider autState { get; set; } = null!;
+    public MudForm MudForm { get; set; }
 }
