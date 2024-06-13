41d13f636aadf0122a8be945bde77e7c0128a72f
Aleksei da Silva <alekseidasilva@hotmail.com>
2024-06-13 07:42:14 +0100

    FIX: Testando Cadastro

--- Dima.Api/Program.cs
+++ Dima.Api/Program.cs
@@ -1,6 +1,7 @@
 using Dima.Api;
 using Dima.Api.Common.Api;
 using Dima.Api.EndPoints;
+using Dima.Core;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.AddConfiguration();
@@ -16,6 +17,9 @@ if (app.Environment.IsDevelopment())
 app.UseCors(ApiConfiguration.CorsPlicyName);
 app.UseSecurity();
 app.MapEndPoints();
+
+Console.WriteLine(Configuration.BackEndUrl);
+Console.WriteLine(Configuration.FrontEndUrl);
 app.Run();
 
 
